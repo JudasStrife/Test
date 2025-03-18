f53ae2cc7dded6e62a368ef9e2493906722d315c
Judas Strife <magashpagamaga@gmail.com>
2025-03-18 13:50:50 +0300

    Inital test

---
+++ src/main/java/com/example/rest_service/GreetingController.java
@@ -0,0 +1,17 @@
+
+package com.example.rest_service;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class GreetingController {
+    public record Greeting(long id, String content){}
+    private static final String template = "Hello, %s and %s!";
+    private final AtomicLong counter = new AtomicLong();
+    @GetMapping("/greeting")
+    public Greeting greeting (@RequestParam(value = "name", defaultValue="World") String name ){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name, name));
+    }
+}
